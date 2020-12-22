c42d7c3f37c66203638f53a1d8d162241fde0640
JamesLeggero <james.m.leggero@gmail.com>
2020-12-22 13:52:09 -0500

    js/html submission and form clear working

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const button = document.getElementById("btn")
+const submission = document.getElementById('submission')
 
-const information = document.getElementById('info')
+submission.addEventListener('submit', event => {
+    event.preventDefault()
+    let stockName = document.getElementById('stockName')
+    console.log(stockName.value)
+    submission.reset()
 
-const addText = text => {
-    const node = document.createElement('li')
-    const hello = document.createTextNode(text)
-    node.appendChild(hello)
-    information.appendChild(node)
-}
-
-
-button.addEventListener('click', () => {
-    addText('Hello')
 })
+
+// const addText = text => {
+//     const ul = document.getElementById('list')
+//     const node = document.createElement('li')
+//     const hello = document.createTextNode(text)
+//     node.appendChild(hello)
+//     // information.appendChild(node)
+//     ul.appendChild(node)
+// }
+
+
+// button.addEventListener('click', () => {
+//     addText('Hello')
+// })
