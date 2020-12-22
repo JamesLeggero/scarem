8aa02a1e060b29cbaa5c43d4074d6ec06a711696
JamesLeggero <james.m.leggero@gmail.com>
2020-12-22 14:35:33 -0500

    requires up with b-ify, api keys working and not uploading. axios printing to DOM and resetting

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+
+const axios = require('axios')
+require('dotenv').config()
+
+const TEST_SECRET = process.env.TEST_SECRET
+const ALPHA = process.env.ALPHAVANTAGE_API
+
+
 const submission = document.getElementById('submission')
 
-submission.addEventListener('submit', event => {
+submission.addEventListener('submit', async event => {
     event.preventDefault()
+    const ul = document.getElementById('list')
+    ul.innerHTML = ''
     let stockName = document.getElementById('stockName')
-    console.log(stockName.value)
+    const response = await axios.get(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${stockName.value}&apikey=${ALPHA}`)
+    const data = response.data
+    const company = [data.Symbol, data.Sector, data.Exchange]
+    
+    for (let i = 0; i < company.length; i++) {
+        const node = document.createElement('li')
+        const text = document.createTextNode(company[i])
+        node.appendChild(text)
+        ul.appendChild(node)
+    }
+    // console.log(data.Symbol)
+    // console.log(data.Sector)
     submission.reset()
 
 })
